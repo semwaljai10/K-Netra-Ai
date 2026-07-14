b64331fd43c8a25248ae35b3134705aedbbc8d38
semwaljai10 <semwaljai10@users.noreply.github.com>
2026-07-14 19:17:01 +0530

    fix: disable Next.js Image Optimization to fix broken logo on Catalyst Slate

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  images: {
+    unoptimized: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   }
 };
 
 export default nextConfig;
+
